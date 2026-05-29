053f72a6a68b6e576e4b9af1d92937d218ce7e45
ftabata30-lab <ftabata30@gmail.com>
2026-05-29 15:28:42 -0300

    finalizando

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-function mudar_conteudo_element(){
+function mudar_paragrafo_1(){
 
-  document.getElementById("h1").innerHTML = "Hello Javascript!";
+  document.getElementById("paragrafo1").style.fontSize = "1.6";
+document.getElementById("paragrafo1").style.color = "green";
+document.getElementById("paragrafo1").style.fontFamily = "Courier new";
+document.getElementById("paragrafo1").style.textAlign = "Left";
+document.getElementById("paragrafo1").style.textTransform = "uppercase";
+}
 
-};
+function mudar_paragrafo_2(){
 
-function mudar_estilo(){
+  document.getElementById("paragrafo2").style.fontSize = "1.3";
+document.getElementById("paragrafo2").style.color = "blue";
+document.getElementById("paragrafo2").style.fontFamily = "Times new Roman";
+document.getElementById("paragrafo2").style.textAlign = "Right";
+document.getElementById("paragrafo2").style.textTransform = "lowercase";
 
-document.getElementById("paragrafo").style.textAlign = "right";
+}
+
+function mudar_paragrafo_3(){
+
+  document.getElementById("paragrafo3").style.fontSize = "1.2";
+document.getElementById("paragrafo3").style.color = "orange";
+document.getElementById("paragrafo3").style.fontFamily = "monospace";
+document.getElementById("paragrafo3").style.textAlign = "center";
+document.getElementById("paragrafo3").style.textTransform = "capitalize";
+
+}
+
+
+function mudar_(){
+
+  document.getElementById("paragrafo3").style.fontSize = "1.2";
+document.getElementById("paragrafo3").style.color = "orange";
+document.getElementById("paragrafo3").style.fontFamily = "monospace";
+document.getElementById("paragrafo3").style.textAlign = "center";
+document.getElementById("paragrafo3").style.textTransform = "capitalize";
+
+}
+
+function mudar_regua_1(){
+
+   document.getElementById("regua3").style.border = "dotted green";
+
+}
+
+function mudar_regua_2(){
+
+   document.getElementById("regua3").style.border = "dashed pink";
+
+}
+
+function mudar_regua_3(){
+
+   document.getElementById("regua3").style.border = "solid blue";
+
 }
